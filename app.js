ab29ffe2e93cc969ef107e83f86589bf5983a193
Lajos Koszti <ajnasz@ajnasz.hu>
2013-07-06 21:15:24 +0200

    Use ini configuration file

--- app.js
+++ app.js
@@ -8,39 +8,55 @@ var express = require('express'),
     PicRoute = require('./routes/pic').PicRoute,
     http = require('http'),
     path = require('path'),
-    cons = require('consolidate');
+    cons = require('consolidate'),
+    IniReader = require('inireader').IniReader;
+    
 
-var app = express();
+function startServer(conf) {
+    var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.engine('html', cons.underscore);
-app.set('view engine', 'html');
+    // all environments
+    app.set('port', conf.server && conf.server.port || 3000);
+    app.set('views', __dirname + '/views');
+    app.engine('html', cons.underscore);
+    app.set('view engine', 'html');
 
-// set environment
-app.set('imagepath', '/uploads/pic/');
+    // set environment
+    app.set('imagepath', conf.uploads.imagepath);
 
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.basicAuth(conf.user.name, conf.user.password));
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' === app.get('env')) {
-    app.use(express.errorHandler());
+    // development only
+    if ('development' === app.get('env')) {
+        app.use(express.errorHandler());
+    }
+
+    picRoute = new PicRoute(app);
+    indexRoute = new IndexRoute(app);
+
+    app.get('/', indexRoute.execIndex);
+    app.get('/lastPic.json', picRoute.execLastPic);
+    app.get('/allPics.json', picRoute.execAllPics);
+    app.put('/pic', picRoute.execAddNewPic);
+
+    var server = http.createServer(app);
+    server.listen(app.get('port'), function () {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
 }
 
-picRoute = new PicRoute(app);
-indexRoute = new IndexRoute(app);
-
-app.get('/', indexRoute.execIndex);
-app.get('/lastPic.json', picRoute.execLastPic);
-app.get('/allPics.json', picRoute.execAllPics);
-app.put('/pic', picRoute.execAddNewPic);
-
-http.createServer(app).listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+var parser = new IniReader();
+parser.on('fileParse', function () {
+    var conf = this.param();
+    startServer(conf);
+    parser = null;
 });
+parser.load('conf.ini');
+
+/* ex: set shiftwidth=4 tabstop=4 expandtab: */
